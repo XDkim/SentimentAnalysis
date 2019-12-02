d6c6fcf5b881357f5fee042b5f091696b1074d20
Bita Golshani <bg2et@virginia.edu>
2019-12-02 16:28:57 -0500

    grab specific candidate tweet - not working

--- sql_calls.php
+++ sql_calls.php
@@ -12,14 +12,32 @@ if (mysqli_connect_errno()) {
 	return null;
 }
 
-$query="SELECT * FROM Tweet";
-$res=mysqli_query($db, $query);
-if (!$res) {
-	die('Error: ' . mysqli_error($db));
+//$query="SELECT * FROM Tweet";
+//$res=mysqli_query($db, $query);
+$stmt = $db -> prepare("SELECT * FROM Candidate where candidate_name=?"); //prepared statement
+// echo $_GET["cand"];
+$cand = !empty($_GET["cand"]);
+$stmt->bind_param("ssdis", $candidate_name, $party_name, $avg_sentiment_score, $total_tweets, $username);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($username);
+
+while($stmt->fetch())
+{
+    echo $username;
 }
+
+
+
+// if (!$res) {
+// 	die('Error: ' . mysqli_error($db));
+// }
+   if(!$stmt) { 
+	   die('Error: ' . mysqli_error($db)); 
+   }
 echo "success";
-while ($row=mysqli_fetch_row($res)) {
-	echo $row[1];
-}
+// while ($row=mysqli_fetch_row($res)) {
+// 	echo $row[1];
+// }
 mysqli_close($db);
 ?>
